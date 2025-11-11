f383e6d5a978e043feb01f2ad55db706178172a8
Zacxta <zacharymurfin@gmail.com>
2025-11-10 23:00:19 -0700

    Final edits complete

--- test_ml.py
+++ test_ml.py
@@ -2,13 +2,14 @@ import pytest
 # TODO: add necessary import
 import os
 import pandas as pd
+import numpy as np
 from ml.data import process_data
 from ml.model import train_model
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 
 
-# TODO: implement the first test. Change the function name and input as needed
 @pytest.fixture
 def test_data():
     project_path = os.getcwd()
@@ -18,14 +19,7 @@ def test_data():
     return data
 
 
-def test_train_model(test_data):
-    """
-    This tests to ensure that the train_model function properly returns
-    a random forest model as expected.
-    """
-    train, test = train_test_split(test_data, test_size=0.2, random_state=42)
-
-    cat_features = [
+cat_features = [
         "workclass",
         "education",
         "marital-status",
@@ -36,26 +30,8 @@ def test_train_model(test_data):
         "native-country",
     ]
 
-    X_train, y_train, _, _ = process_data(
-        train, cat_features, training=True, label="salary"
-        )
 
-    model = train_model(X_train, y_train)
-    assert isinstance(model, RandomForestClassifier), \
-        "Model is not a RandomForestClassifier"
-
-
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
-
-
-# TODO: implement the third test. Change the function name and input as needed
-def test_train_test_data_types():
+def test_train_test_data_types(test_data):
     """
     This test checks to ensure that the train and test split are
     actually pandas dataframes after the split.
@@ -67,3 +43,35 @@ def test_train_test_data_types():
         "Train output is not a pandas dataframe."
     assert isinstance(test, pd.DataFrame), \
         "Test output is not a pandas dataframe."
+
+
+def test_process_data_return_check(test_data):
+    """
+    This test checks the output types from the process_data function.
+    """
+    train, _ = train_test_split(test_data, test_size=0.2, random_state=42)
+    X_train, y_train, encoder, lb = process_data(
+        train, cat_features, training=True, label="salary"
+        )
+    assert isinstance(X_train, np.ndarray), \
+        "X output is not a numpy array."
+    assert isinstance(y_train, np.ndarray), \
+        "y output is not a numpy array."
+    assert isinstance(encoder, OneHotEncoder), \
+        "Encoder output error."
+    assert isinstance(lb, LabelBinarizer), \
+        "lb output error."
+
+
+def test_train_model(test_data):
+    """
+    This tests to ensure that the train_model function properly returns
+    a random forest model as expected.
+    """
+    train, test = train_test_split(test_data, test_size=0.2, random_state=42)
+    X_train, y_train, _, _ = process_data(
+        train, cat_features, training=True, label="salary"
+        )
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier), \
+        "Model is not a RandomForestClassifier"
